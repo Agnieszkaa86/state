42addfe3bad8401f42ff84c5b8e8cd6433831188
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-03 08:12:42 +0100

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,62 @@
-import MyForm from "./MyForm";
+import { Component } from "react";
+import {fetchArticlesByTopic} from "../services/api";
+import ArticleList from "./ArticleList";
 
 
-const App = () => (
-  <>
-    <h1>React Forms!!!!</h1>
-    <MyForm label="Nasz formularz" formSubmissionHandler={(data) => {
-      console.log(">>> App Is Calling!!", data)
-    }} />
-  </>
-)
+class App extends Component {
+  state = {
+    articles: [],
+    topic: "react",
+    isLoading: false,
+    error: null
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    
+    try {
+      const articles = await fetchArticlesByTopic(this.state.topic)
+         this.setState({articles, isLoading:false})
+    } catch (error) {
+      console.log("error: ", error)
+      this.setState({error})
+    } finally {
+      this.setState({isLoading:false})
+    }
+  }
+
+  updateArticles = (topic) => {
+    console.log("updatedArticles: ", topic)
+    this.setState({ topic, isLoading: true }, async () => {
+      const articles = await fetchArticlesByTopic(this.state.topic)
+      this.setState({ articles, isLoading: false });
+    })
+  }
+  render() {
+    const { articles, isLoading, error} = this.state;
+    return (
+      <>
+        <h1>Hello!!!</h1>
+
+        <button onClick={() => this.updateArticles("react")}>React</button>
+        <button onClick={()=> this.updateArticles("css")}>CSS</button>
+
+        {error && <div>Something went wrong... :( </div>}
+        
+        {isLoading ? (
+          <p>Please wait....</p>
+        ) : (
+            <div>
+              {articles.length > 0 && (
+                <ArticleList articles={articles} />
+              )}
+            </div>
+        )};
+        
+       
+      </>
+    );
+  }
+}
 
 export default App;
